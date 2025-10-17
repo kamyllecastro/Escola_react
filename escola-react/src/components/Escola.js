c9dce1a5cdef464542fe93f05e6da1014a9fabf5
kamyllecastro <kamylle.castro.santos@escola.pr.gov.br>
2025-10-17 10:38:46 -0300

    commit

---
+++ escola-react/src/components/Escola.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+export default function Escola() {
+    return (
+        <section id="escola" className="p-10 text-center bg-gray-100">
+            <h1 className="text-3xl font-bold">SOBRE A ESCOLA</h1>
+            <p className="mt-4 text-gray-700">AQUI VOCÊ ENCONTRA INFORMAÇÕES SOBRE A ESCOLA</p>
+        </section>
+    );
+}
