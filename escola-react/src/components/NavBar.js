014e05254e186a0ac2b8a9bc9410d64ac16f1662
kamyllecastro <kamylle.castro.santos@escola.pr.gov.br>
2025-10-17 10:38:59 -0300

    commit

--- escola-react/src/components/NavBar.js
+++ escola-react/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Navbar(){
+export default function NavBar(){
     return (
         <nav className="flex justify-center gap-6 bg-gray-200 p-4 shadow-md">
             <a href="#home" className="hover: text-blue-600">Home</a>
