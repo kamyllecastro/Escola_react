ba8979dcd5d909d1c715d8f1d673ea4e907ce499
kamyllecastro <kamylle.castro.santos@escola.pr.gov.br>
2025-10-17 10:38:53 -0300

    commit

---
+++ escola-react/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+return(
+    <footer className="footer">
+        <div className="footer-bottom">
+            <p>Otalípio Pereira de Andrade</p>
+        </div>
+    </footer>
+);
+}
