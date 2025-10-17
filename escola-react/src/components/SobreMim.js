8f7df3bf9f59432fdb6ef1a8e438c72e6d849c29
kamyllecastro <kamylle.castro.santos@escola.pr.gov.br>
2025-10-17 10:39:05 -0300

    commit

---
+++ escola-react/src/components/SobreMim.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SobreMim() {
+    return (
+        <section id="mim" className="p-10 text-center">
+            <h1 className="text-3xl font-bold">SOBRE MIM</h1>
+            <p className="mt-4 text-gray-700">INFORMAÇÕES SOBRE MIM</p>
+        </section>
+    );
+}
